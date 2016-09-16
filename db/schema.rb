7a985fda76fee5af39ea5e942bb37047e5f30f32
Sky Rousse <skyrousse@gmail.com>
2016-09-16 11:38:57 -0700

    uncomment confirmable code to use Devise mailers

--- db/schema.rb
+++ db/schema.rb
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 20160916170910) do
     t.inet     "last_sign_in_ip"
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.integer  "failed_attempts",        default: 0,  null: false
     t.string   "unlock_token"
     t.datetime "locked_at"
