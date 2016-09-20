10b860e9b6bb00ce3b6599248b5189e10f2617f8
sr <skyrousse@gmail.com>
2016-09-19 22:59:37 -0700

    update user model to use avatars

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:street_address, :community, :email, :password, :password_confirmation, :first_name, :last_name, :dob, :sex, :remember_me) }
     devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:email, :password, :password_confirmation, :remember_me) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:street_address, :community, :email, :password, :first_name, :last_name, :dob, :sex, :remember_me, :about, :skills, :zip_code, :likes, :current_password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:street_address, :community, :email, :password, :password_confirmation, :avatar, :first_name, :last_name, :dob, :sex, :remember_me, :about, :skills, :zip_code, :likes, :current_password) }
   end
 end
