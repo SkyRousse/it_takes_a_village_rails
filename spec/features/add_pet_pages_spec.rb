5d2edfd847bd5ce72d793313b25e4dda05602f25
sr <skyrousse@gmail.com>
2016-09-29 16:46:32 -0700

    update integration spec for adding pets

--- spec/features/add_pet_pages_spec.rb
+++ spec/features/add_pet_pages_spec.rb
@@ -3,10 +3,17 @@ require 'rails_helper'
 describe "the add a pet path" do
   it "will allow a user to see all pets", js: true do
     user = FactoryGirl.create(:user)
-    FactoryGirl.create(:pet_category)
+    FactoryGirl.create(:pet_category, name: "walks")
+    FactoryGirl.create(:pet_category, name: "feeding")
+    FactoryGirl.create(:pet_category, name: "day care")
     login_as(user)
     visit root_path
     click_link 'Pets'
+    click_link 'Add New'
+    fill_in "Name", :with => "Peluche"
+    fill_in "Species", :with => "Dog"
+    fill_in "Age", :with => "old"
+    fill_in "About", :with => "Best dog ever"
     select 'walks'
     click_on 'Create Pet'
     expect(page).to have_content('Pet saved successfully')
--- spec/features/show_pets_pages_spec.rb
+++ spec/features/show_pets_pages_spec.rb
@@ -3,20 +3,25 @@ require 'rails_helper'
 describe "the add a pet path" do
   it "will allow a user to see all pets", js: true do
     user = FactoryGirl.create(:user)
-    FactoryGirl.create(:pet_category)
+    FactoryGirl.create(:pet_category, name: "walks")
+    FactoryGirl.create(:pet_category, name: "feeding")
+    FactoryGirl.create(:pet_category, name: "day care")
     login_as(user)
     visit root_path
     click_link 'Pets'
+    click_link 'Add New'
     select 'walks'
     click_on 'Create Pet'
     expect(page).to have_content('Pet saved successfully')
   end
   it "will allow a user to sort pets", js: true do
     user = FactoryGirl.create(:user)
-    FactoryGirl.create(:pet_category)
+    FactoryGirl.create(:pet_category, name: "walks")
+    FactoryGirl.create(:pet_category, name: "feeding")
+    FactoryGirl.create(:pet_category, name: "day care")
+    FactoryGirl.create(:pet)
     login_as(user)
-    visit root_path
-    click_link 'Pets'
+    visit pets_path
     select 'walks'
     click_on 'Create Pet'
     expect(page).to have_content('Pet saved successfully')
