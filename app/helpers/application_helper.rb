f1337212b30ef963a1bda493e526c20179ab40d6
Sky Rousse <skyrousse@gmail.com>
2016-10-04 14:35:08 -0700

    add helper methods to make body class and title dynamic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,10 @@ module ApplicationHelper
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
     link_to title, url_for(:sort => column, :direction => direction, :page => nil), {:class => css_class, :remote => true}
   end
+  def title(text)
+    content_for :title, text
+  end
+  def body_class
+    [controller_name, action_name].join('-')
+  end
 end
